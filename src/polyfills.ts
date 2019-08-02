aa7c9d0afc76900befa48452544bd1ef32ff8dd2
Matt Strom <matthew.strom@evolv.ai>
2019-08-02 09:34:21 -0700

    fix: address issue with async iterator polyfill
    Node > 8.10 does not permit writing to Symbol.asyncIterator as polyfill does

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,1 +1,3 @@
-(Symbol as any).asyncIterator = Symbol.asyncIterator || Symbol.for('Symbol.asyncIterator');
+if (!Symbol.asyncIterator) {
+	(Symbol as any).asyncIterator = Symbol.for('Symbol.asyncIterator');
+}

fc8f05d4b19da3536d1fe178f49a4d5194f519d0
Matt Strom <matt.strom@evolv.ai>
2020-05-19 11:39:49 -0700

    build: add linting to pre-commit hook

--- src/utils/load-module.ts
+++ src/utils/load-module.ts
@@ -3,15 +3,15 @@ import { resolve } from 'path';
 export async function loadModule(path: string): Promise<Function> {
 	const regex = /(.+)\.(.+)/;
 	const match = regex.exec(path);
-	
+
 	if (!match) {
 		throw new Error('Could not find module');
 	}
-	
+
 	const [, modulePath, functionName] = match;
 	const absPath = resolve(modulePath);
-	
+
 	const module = await import(absPath);
-	
+
 	return module[functionName];
 }
